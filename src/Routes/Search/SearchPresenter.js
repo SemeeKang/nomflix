a2889132f409babe67abc66ca70032e39e8c89c8
Semee Kang <semee.kang01@gmail.com>
2021-06-21 10:37:18 -0400

    worked searching by term

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,8 +1,53 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
+import { tvApi } from "../../api";
 
-const SearchPresenter = ({movieResults, tvResults, searchTerm, loading, error, handleSubmit}) => null;
+const Container = styled.div`
+    padding: 0px 20px;
+`;
+
+const Form = styled.form`
+    margin-bottom: 50px;
+    width: 100%;
+`;
+
+const Input = styled.input`
+    all:unset;
+    font-size: 28px;
+    width: 100%;
+
+`;
+
+const SearchPresenter = ({movieResults, tvResults, searchTerm, loading, error, handleSubmit, updateTerm}) => (
+    <Container>
+        <Form onSubmit={handleSubmit}>
+            <Input placeholder="Search Movies or TV Shows..." value={searchTerm} onChange={updateTerm}>
+
+            </Input>
+        </Form>
+        {loading ? <Loader/> : <>
+            {movieResults && movieResults.length > 0 && (
+                <Section title="Movie Results"> {
+                   movieResults.map(movie => (
+                       <span key={movie.id}>{movie.title}</span>
+                   ))
+                }</Section>
+            )}
+            {tvResults && tvResults.length > 0 && (
+                <Section title="TV Results"> {
+                    tvResults.map(show => (
+                       <span key={show.id}>{show.name}</span>
+                   ))
+                }</Section>
+            )}      
+        </>}
+    </Container>
+);
+
+
 
 SearchPresenter.protoTypes = {
     movieResults: PropTypes.array,
@@ -10,7 +55,8 @@ SearchPresenter.protoTypes = {
     searchTerm: PropTypes.string,
     loading: PropTypes.bool.isRequired,
     error: PropTypes.string,
-    handleSubmit: PropTypes.func.isRequireds
+    handleSubmit: PropTypes.func.isRequireds,
+    updateTerm: PropTypes.func.isRequired
 };
 
 export default SearchPresenter;
